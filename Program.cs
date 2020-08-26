e69f5de5469626f63396545d23bdc9e21f445e3e
Cheena Malhotra <v-chmalh@microsoft.com>
2020-08-26 12:02:52 -0700

    New changes to parallel test various libraries

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Azure.Core;
+using Azure.Identity;
 using Microsoft.Azure.Services.AppAuthentication;
 using System.Threading;
 using System.IO;
@@ -13,88 +15,126 @@ namespace MSITokenTest
 {
     class Program
     {
-        /// <summary>
-        /// Runs test application with connection string in argument.
-        /// Requires Environment variable "MSI_TEST_CONN_STRING" to contain connection string.
-        /// </summary>
-        /// <param name="args"></param>
+        private const string expiry = "expires_on";
+        private const string azureSqlResourceId = "https://database.windows.net/";
+
         static void Main(string[] args)
         {
-            string conenctionString = Environment.GetEnvironmentVariable("MSI_TEST_CONN_STRING");
-            string accessToken = null;
-            string expiry = "expires_on";
+            string connectionString = Environment.GetEnvironmentVariable("MSI_TEST_CONN_STRING");
+            string[] accessTokens = new string[3];
 
             while (true)
             {
-                var azureServiceTokenProvider = new AzureServiceTokenProvider();
-                var tempAccessToken = azureServiceTokenProvider.GetAccessTokenAsync("https://database.windows.net/").GetAwaiter().GetResult();
-                if (accessToken != tempAccessToken)
+                accessTokens[0] = GetAccessTokenFromIMDS();
+                accessTokens[1] = GetAccessTokenFromAppAuth();
+                accessTokens[2] = GetAccessTokenFromAzureIdentity();
+
+                for (int i = 0; i < accessTokens.Length; i++)
                 {
-                    Console.WriteLine(DateTime.UtcNow + " | NEW ACCESS TOKEN RECEIVED BY AZURE APP AUTH");
-                    PrintAccessTokenExpiry(tempAccessToken);
-                    accessToken = tempAccessToken;
-                }
-
-                // Build request to acquire managed identities for Azure resources token
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://database.windows.net/");
-                request.Headers["Metadata"] = "true";
-                request.Method = "GET";
-
-                try
-                {
-                    // Call /token endpoint
-                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-                    // Pipe response Stream to a StreamReader, and extract access token
-                    StreamReader streamResponse = new StreamReader(response.GetResponseStream());
-                    string stringResponse = streamResponse.ReadToEnd();
-                    var imdsAccessToken = stringResponse.Substring(17, stringResponse.IndexOf('"', 17) - 17);
-                    if (accessToken != imdsAccessToken)
+                    try
                     {
-                        Console.WriteLine("ATTENTION! DIFFERENT ACCESS TOKEN RECEIVED FROM IMDS");
-                        Console.WriteLine("Azure Identity Access Token:");
-                        PrintAccessTokenExpiry(accessToken);
-                        Console.WriteLine("IMDS Access Token:");
-                        PrintAccessTokenExpiry(imdsAccessToken);
-                    }
-                    var expiresin = stringResponse.Substring(stringResponse.IndexOf(expiry) + expiry.Length + 3, 10);
-                    var expiryTime = DateTime.UnixEpoch.AddSeconds(long.Parse(expiresin));
-                    Console.WriteLine("********IMDS********");
-                    PrintAccessTokenExpiry(imdsAccessToken);
-                    Console.WriteLine("IMDS Expires On: " + expiryTime);
-                    Console.WriteLine("********************");
-                }
-                catch (Exception e)
-                {
-                    string errorText = String.Format("{0} \n\n{1}", e.Message, e.InnerException != null ? e.InnerException.Message : "Acquire token failed");
-                    Console.WriteLine(e.Message);
-                }
-
-                using (SqlConnection conn = new SqlConnection(conenctionString))
-                {
-                    conn.AccessToken = accessToken;
-                    conn.Open();
-                    Console.WriteLine(DateTime.UtcNow + " | Pooling: no | " + "Connected");
-                    using (SqlCommand cmd = conn.CreateCommand())
-                    {
-                        cmd.CommandText = "SELECT @@VERSION";
-                        using (var reader = cmd.ExecuteReader())
+                        using (SqlConnection conn = new SqlConnection(connectionString))
                         {
-                            while (reader.Read())
+                            conn.AccessToken = accessTokens[i];
+                            conn.Open();
+                            Console.WriteLine(DateTime.UtcNow + $" Connected with Access token from {GetProvider(i)}");
+                            using (SqlCommand cmd = conn.CreateCommand())
                             {
-                                Console.WriteLine(reader.GetValue(0));
+                                cmd.CommandText = "SELECT @@VERSION";
+                                using (var reader = cmd.ExecuteReader())
+                                {
+                                    while (reader.Read())
+                                    {
+                                        Console.WriteLine(DateTime.UtcNow + " " + reader.GetValue(0));
+                                    }
+                                }
                             }
                         }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(DateTime.UtcNow + " " + e.ToString());
+                        Console.WriteLine(DateTime.UtcNow + $" ACCESS TOKEN PROVIDER: {GetProvider(i)}");
+                        Console.WriteLine(DateTime.UtcNow + " " + accessTokens[i]);
+                        PrintAccessTokenExpiry(accessTokens[i]);
                     }
                 }
                 Thread.Sleep(5 * 60 * 1000);
             }
         }
 
+        private static string GetProvider(int i)
+        {
+            switch (i)
+            {
+                case 0: return "IMDS Service";
+                case 1: return "App Auth Library";
+                case 2: return "Azure Identity";
+            }
+            return null;
+        }
+
+        private static string GetAccessTokenFromIMDS()
+        {
+            // Build request to acquire managed identities for Azure resources token
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://database.windows.net/");
+            request.Headers["Metadata"] = "true";
+            request.Method = "GET";
+
+            try
+            {
+                // Call /token endpoint
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                // Pipe response Stream to a StreamReader, and extract access token
+                StreamReader streamResponse = new StreamReader(response.GetResponseStream());
+                string stringResponse = streamResponse.ReadToEnd();
+                var imdsAccessToken = stringResponse.Substring(17, stringResponse.IndexOf('"', 17) - 17);
+                var expiresin = stringResponse.Substring(stringResponse.IndexOf(expiry) + expiry.Length + 3, 10);
+                var expiryTime = DateTime.UnixEpoch.AddSeconds(long.Parse(expiresin));
+                Console.WriteLine(DateTime.UtcNow + $" ******** IMDS ********");
+                PrintAccessTokenExpiry(imdsAccessToken);
+                Console.WriteLine(DateTime.UtcNow + $" IMDS Expires On: " + expiryTime);
+                Console.WriteLine(DateTime.UtcNow + $" ********************");
+                return imdsAccessToken;
+            }
+            catch (Exception e)
+            {
+                string errorText = String.Format("{0} \n\n{1}", e.Message, e.InnerException != null ? e.InnerException.Message : "Acquire token failed");
+                Console.WriteLine(DateTime.UtcNow + " " + e.Message);
+                return null;
+            }
+        }
+
+        private static string GetAccessTokenFromAppAuth()
+        {
+            var token = new AzureServiceTokenProvider().GetAccessTokenAsync(azureSqlResourceId).GetAwaiter().GetResult();
+
+            Console.WriteLine(DateTime.UtcNow + $" ******** App Auth Library ********");
+            PrintAccessTokenExpiry(token);
+            Console.WriteLine(DateTime.UtcNow + $" ********************");
+
+            return token;
+        }
+
+        private static string GetAccessTokenFromAzureIdentity()
+        {
+            var tokenCredential = new DefaultAzureCredential();
+            var context = new TokenRequestContext(new string[] { azureSqlResourceId });
+            var token = tokenCredential.GetTokenAsync(context).GetAwaiter().GetResult();
+
+            Console.WriteLine(DateTime.UtcNow + $" ******** Azure Identity Library ********");
+            PrintAccessTokenExpiry(token.Token);
+                Console.WriteLine(DateTime.UtcNow + $" Azure Identity Expires On: " + token.ExpiresOn);
+            Console.WriteLine(DateTime.UtcNow + $" ********************");
+
+            return token.Token;
+        }
+
         private static void PrintAccessTokenExpiry(string accessToken)
         {
             var payload = JObject.Parse(ReadToken(accessToken)).GetValue("Payload");
-            Console.Out.WriteLine($"JWT Expires On:  " + DateTime.UnixEpoch.AddSeconds((long)payload[4]["Value"]));
+            Console.Out.WriteLine(DateTime.UtcNow + $" JWT Token Expires On:  " + DateTime.UnixEpoch.AddSeconds((long)payload[4]["Value"]));
         }
         public static string ReadToken(string jwtInput)
         {
